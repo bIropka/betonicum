7d20d88287bbca551599cdfca00f6706012c7989
Iegor Stetsiura <biropka@gmail.com>
2016-08-10 00:55:35 +0300

    v 0.1

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
 
+    $('.burger').click(function() {
+        $(this).toggleClass('active');
+        $('nav ul').slideToggle();
+    });
+
+    /****************
+     **** sliders ***
+     ****************/
+
+    $('.slider-header').slick({
+        arrows: false,
+        dots: true
+    });
 });
