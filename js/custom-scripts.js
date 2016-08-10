6ab747cba7a2cbe40a2d5d85d4c4bda7d904b67e
bIropka <biropka@gmail.com>
2016-08-10 16:21:33 +0300

    v 0.1

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,8 +1,71 @@
 $(document).ready(function () {
 
+    /****************
+     ***** init *****
+     ****************/
+
+    if ($(window).width() > '1180'){
+        $('nav>ul').css('display', 'block');
+        $('.burger').removeClass('active');
+    } else {
+        $('nav>ul').css('display', 'none');
+    }
+
+    $(window).resize(function(){
+        if ($(window).width() > '1180'){
+            $('nav>ul').css('display', 'block');
+            $('.burger').removeClass('active');
+        } else {
+            $('nav>ul').css('display', 'none');
+        }
+    });
+
+    $(document).click(function() {
+
+        $target = $(event.target);
+
+        if (!$target.closest($('nav>ul>li')).length){
+            $('nav>ul>li>a').removeClass('active');
+            $('nav>ul>li>ul').fadeOut();
+        }
+
+    });
+
+    /****************
+     **** others *****
+     ****************/
+
     $('.burger').click(function() {
+
         $(this).toggleClass('active');
-        $('nav ul').slideToggle();
+        $('nav>ul').slideToggle();
+
+    });
+
+    $('nav>ul>li').hover(function() {
+
+        if($(window).width() > 1180) {
+            $(this).children('a').toggleClass('active');
+            $(this).children('ul').stop().fadeToggle();
+        }
+
+    });
+    $('nav>ul>li').click(function() {
+
+        if($(window).width() < 1181) {
+
+            if($(this).children('a').hasClass('active')) {
+                $('nav>ul>li>a').removeClass('active');
+                $('nav>ul>li>ul').fadeOut();
+            } else {
+                $('nav>ul>li>a').removeClass('active');
+                $('nav>ul>li>ul').fadeOut();
+                $(this).children('a').addClass('active');
+                $(this).children('ul').stop().fadeIn();
+            }
+
+        }
+
     });
 
     /****************
